4a5187c4967ba28e0b314be90e9676a429a88122
Kirsten Cassidy <mantismamita@gmail.com>
2015-11-22 21:44:12 +0100

    adds base styling and structures blog roll

--- functions.php
+++ functions.php
@@ -5,13 +5,14 @@ include_once( get_template_directory() . '/lib/init.php' );
 //* Child theme (do not remove)
 define( 'CHILD_THEME_NAME', 'Even Blanker Genesis' );
 define( 'CHILD_THEME_URL', 'http://www.studiopress.com/' );
-define( 'CHILD_THEME_VERSION', '2.2.2' );
+define( 'CHILD_THEME_VERSION', '2.2.3.1' );
 
 //* Enqueue Google Fonts
 add_action( 'wp_enqueue_scripts', 'genesis_sample_google_fonts' );
 function genesis_sample_google_fonts() {
 
 	wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Lato:300,400,700', array(), CHILD_THEME_VERSION );
+	wp_enqueue_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css', array(), CHILD_THEME_VERSION );
 
 }
 
@@ -40,10 +41,46 @@ remove_action( 'genesis_after_header', 'genesis_do_nav' );
 add_action( 'genesis_header_right', 'genesis_do_nav', 1 );
 
 //* Remove entry content from genesis loop
-
 add_action( 'get_header', 'remove_titles_blog_page' );
+
 function remove_titles_blog_page() {
 	if ( is_home() && is_front_page() ) {
 		remove_action( 'genesis_entry_content', 'genesis_do_post_content' );
 	}
+}
+
+//* Changes wording
+add_filter( 'genesis_post_meta', 'kc_entry_meta_filter' );
+//add_filter( 'genesis_post_meta', 'kc_entry_meta_labels' );
+
+function kc_entry_meta_filter( $post_meta ) {
+	$post_meta = '[post_categories sep="" before="Categories: "][post_tags sep="" before="Tags: "]';
+	return $post_meta;
+}
+function kc_entry_meta_labels( $post_meta ) {
+	$post_meta = '[post_categories "][post_tags ]';
+	return $post_meta;
+}
+
+add_filter( 'genesis_post_info', 'post_info_filter' );
+function post_info_filter($post_info) {
+	if ( !is_page() ) {
+		$post_info = '[post_date] by [post_author_posts_link] [post_comments zero=" Comment " one=" 1 " more=" % "]';
+		return $post_info;
+	}}
+
+
+//add_filter( 'genesis_post_comments_shortcode', 'prefix_post_comments_shortcode' );
+/**
+ * Amend the post comments shortcode to add extra markup for styling.
+ *
+ * @author Gary Jones
+ * @link   http://gamajo.com/style-comment-number/
+ *
+ * @param string $output HTML markup.
+ *
+ * @return string HTML markup.
+ */
+function prefix_post_comments_shortcode( $output ) {
+	return preg_replace( '/#comments"\>(\d+) Comment/', '#comments"><span class="entry-comments-number">$1</span> Comment', $output );
 }
